f39a6c49962fb43e77c06ac5d4c10ecb89490d22
Antonio Pereira <anthonyesany0712@gmail.com>
2024-03-28 23:13:32 -0300

    feat: add views home

--- twitter_project/settings.py
+++ twitter_project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'twitter.apps.TwitterConfig',
+    'django.contrib.humanize',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
