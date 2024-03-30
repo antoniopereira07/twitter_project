f300c372ca72a6de7a92aa1e11e34ff3c8998bb8
Antonio Pereira <anthonyesany0712@gmail.com>
2024-03-29 23:36:26 -0300

    feat: add post

--- twitter/forms.py
+++ twitter/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Post
 
 class UserRegisterForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
@@ -14,6 +15,13 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['first_name', 'username', 'email', 'password1', 'password2']
+
+class PostForm(forms.ModelForm):
+    content = forms.CharField(widget=forms.Textarea(attrs={'class':'form-control w-100', 'id':'cotentsBox', 'rows':'3', 'placeholder':'O que está acontecendo?'}))
+
+    class Meta:
+        model = Post
+        fields = ['content']
 
 
 
--- twitter/views.py
+++ twitter/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render, redirect
 from .models import Profile, Post
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, PostForm
 
 def home(request):
     posts = Post.objects.all()
-    context = {'posts':posts}
+    form = PostForm()
+
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+            return redirect('home')
+        else:
+            form = PostForm()
+
+    context = {'posts': posts, 'form': form}
     return render(request, 'twitter/newsfeed.html', context)
 
 def register(request):
