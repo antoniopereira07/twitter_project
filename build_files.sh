c2b43f90264f69626d8012179fecb516efd17414
Antonio Pereira <anthonyesany0712@gmail.com>
2024-04-11 22:21:28 -0300

    add file

--- build_files.sh
+++ build_files.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-# Criação e ativação do ambiente virtual
-python3.9 -m venv venv
-source venv/bin/activate
-
-# Certifique-se de que o pip está instalado no ambiente virtual
+# Certifique-se de que o pip está instalado
 python3.9 -m ensurepip
 
-# Instalação das Dependências no ambiente virtual
+# Instalação das Dependências
 python3.9 -m pip install -r requirements.txt
 
 # Migrações do Banco de Dados
 python3.9 manage.py migrate
+
+# Verifica se o diretório 'staticfiles_build/static' existe e o cria, se necessário
+if [ ! -d "/vercel/path0/staticfiles_build/static" ]; then
+    mkdir -p /vercel/path0/staticfiles_build/static
+fi
 
 # Coleta de Arquivos Estáticos
 python3.9 manage.py collectstatic --noinput --clear
