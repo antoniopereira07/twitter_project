b02b5e3151d683eff9c87181d96d966c7192dd4f
Antonio Pereira <anthonyesany0712@gmail.com>
2024-04-11 21:44:40 -0300

    add collectstatic

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-# Certifique-se de que o pip está instalado
+# Criação e ativação do ambiente virtual
+python3.9 -m venv venv
+source venv/bin/activate
+
+# Certifique-se de que o pip está instalado no ambiente virtual
 python3.9 -m ensurepip
 
-# Instalação das Dependências
+# Instalação das Dependências no ambiente virtual
 python3.9 -m pip install -r requirements.txt
 
 # Migrações do Banco de Dados
